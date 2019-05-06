19c991505d2478ee8fcfea04a782b10caa7ad5eb
Oleh Lutsenko <oleh.lutsenko@ad.exopen.se>
2019-05-06 05:41:10 +0200

    init firebase in the project

---
+++ src/firebase/firebase.js
@@ -0,0 +1,35 @@
+import app from 'firebase/app';
+import 'firebase/database';
+
+const config = {
+    apiKey: "AIzaSyCvwmNhhs-1PEF9Qk1gOYNpp-m65-0JJNY",
+    authDomain: "exopen-c2af4.firebaseapp.com",
+    databaseURL: "https://exopen-c2af4.firebaseio.com",
+    projectId: "exopen-c2af4",
+    storageBucket: "exopen-c2af4.appspot.com",
+    messagingSenderId: "1003479661787"
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+
+        this.db = app.database();
+        this.auth = app.auth();
+    }
+
+    // *** Auth API ***
+    createUserWithEmailAndPassword = (email, password) => 
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    signInWithEmailAndPassword = (email, password) => 
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    signOut = () => this.auth.signOut();
+
+    sendPasswordResetEmail = email => this.auth.sendPasswordResetEmail(email);
+
+    updatePassword = password => this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
---
+++ src/firebase/index.js
@@ -0,0 +1,6 @@
+import Firebase from './firebase';
+import FirebaseContext from './context';
+
+export default Firebase;
+
+export { FirebaseContext };
