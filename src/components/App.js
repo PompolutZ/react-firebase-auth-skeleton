1cdf5c73ddc995fdff7dab813094ebb10d4d2cbf
Oleh Lutsenko <oleh.lutsenko@ad.exopen.se>
2019-05-06 05:38:56 +0200

    add init routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navigation from './Navigation';
+import Landing from './Landing';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import PasswordForget from './PasswordForget';
+import Home from './Home';
+import Account from './Account';
+import Admin from './Admin';
+import * as ROUTES from '../constants/routes';
+
 
 function App() {
     return (
+      <Router>
         <div>
-            App
+          <Navigation />
+
+          <hr />
+
+          <Route exact path={ROUTES.LANDING} component={Landing} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.SIGN_IN} component={SignIn} />
+          <Route
+            path={ROUTES.PASSWORD_FORGET}
+            component={PasswordForget}
+          />
+          <Route path={ROUTES.HOME} component={Home} />
+          <Route path={ROUTES.ACCOUNT} component={Account} />
+          <Route path={ROUTES.ADMIN} component={Admin} />
         </div>
-    )
+      </Router>
+    );
 }
 
 export default App;
