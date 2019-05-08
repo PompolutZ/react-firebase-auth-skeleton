0d10647ff1ee591238dd1ed95da19ce656bcc68e
Oleh Lutsenko <oleh.lutsenko@ad.exopen.se>
2019-05-08 14:34:50 +0200

    move config to separate file

---
+++ src/firebase/config.js
@@ -0,0 +1,5 @@
+const config = {
+    // COPY YOUR FIREBASE PROJECT CONFIG PROPERTIES HERE
+};
+
+export default config;
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,7 @@
 import app from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
-
-const config = {
-    apiKey: "AIzaSyCvwmNhhs-1PEF9Qk1gOYNpp-m65-0JJNY",
-    authDomain: "exopen-c2af4.firebaseapp.com",
-    databaseURL: "https://exopen-c2af4.firebaseio.com",
-    projectId: "exopen-c2af4",
-    storageBucket: "exopen-c2af4.appspot.com",
-    messagingSenderId: "1003479661787"
-};
+import config from './config';
 
 class Firebase {
     constructor() {
