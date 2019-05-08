357e3f08e803c7550475d1452dae1306028bb839
Oleh Lutsenko <oleh.lutsenko@ad.exopen.se>
2019-05-08 14:38:22 +0200

    add config to ignore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,10 @@
 const config = {
-    // COPY YOUR FIREBASE PROJECT CONFIG PROPERTIES HERE
+    apiKey: "AIzaSyCvwmNhhs-1PEF9Qk1gOYNpp-m65-0JJNY",
+    authDomain: "exopen-c2af4.firebaseapp.com",
+    databaseURL: "https://exopen-c2af4.firebaseio.com",
+    projectId: "exopen-c2af4",
+    storageBucket: "exopen-c2af4.appspot.com",
+    messagingSenderId: "1003479661787"
 };
 
 export default config;
